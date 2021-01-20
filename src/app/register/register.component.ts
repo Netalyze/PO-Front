fc998be5a3b7b3142cdca823e3b9071ee74cfa6b
Piotr Rasiński <246764@student.pwr.edu.pl>
2021-01-20 18:08:37 +0100

    Register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { first } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(private formbuilder: FormBuilder, 
+              private auth: AuthService, 
+              private router: Router) {
+    if (this.auth.isLoggedIn) {
+      this.router.navigate(['/']);
+    }
+   }
 
   ngOnInit(): void {
+    this.registerForm = this.formbuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, Validators.minLength(5)]],
+      password: ['', Validators.required],
+      acceptTerms: [false, Validators.requiredTrue]
+    });
   }
 
+  get form() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.auth.register(this.form.email.value, this.form.username.value, this.form.password.value)
+      .pipe(first())
+      .subscribe(
+        () => { this.router.navigate(['/login']); }, // Może być do zmiany
+        err => {
+          this.loading = false;
+          console.log(err);
+        }
+      )
+  }
 }
