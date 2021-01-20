b4eb07e75c1a32dafcce0de31b87c9f76892c46a
Piotr Rasiński <246764@student.pwr.edu.pl>
2021-01-20 12:53:05 +0100

    Prosta logika logowania

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { first } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder, 
+              private auth: AuthService, 
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  get form() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.auth.login(this.form.username.value, this.form.password.value)
+      .pipe(first())
+      .subscribe(data => {
+        this.router.navigate(['/']);
+      }, 
+      err => {
+        this.loading = false;
+        console.log(err);
+      })
   }
 
 }
