3bc0f309ee1df326a059ccefad5d6f57ada16cc4
Piotr Rasiński <246764@student.pwr.edu.pl>
2021-01-20 14:33:21 +0100

    Usuniecie observables, zostawienie tylko localStorage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,36 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from '../datamodels/user';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private currentUserSubject!: BehaviorSubject<User>;
-  public currentUser!: Observable<User>;
 
-  constructor(private http: HttpClient) {
-    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser') || '{}'));
-    this.currentUser = this.currentUserSubject.asObservable();
-  }
-
-  public get currentUserValue(): User {
-    return this.currentUserSubject.value;
-  }
+  constructor(private http: HttpClient) {}
 
   login(username: string, password: string) {
-    return this.http.post<any>('API URL', { username, password})
+    return this.http.post<any>(`${environment.apiUrl}`, { username, password})
       .pipe(map(user => {
         localStorage.setItem('currentUser', JSON.stringify(user));
-        this.currentUserSubject.next(user);
         return user;
       }));
   }
 
   logout() {
     localStorage.removeItem('currentUser');
-    this.currentUserSubject.next('{}'); // Not sure
   }
 }
